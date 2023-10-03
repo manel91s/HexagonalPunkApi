a85f24fb6d7a0f4ac27975d7104fddfc53f6e890
Manel <manel.aguilera91@gmail.com>
2023-10-04 00:03:48 +0200

    add redis dependencies and cache objects

--- src/Infrastructure/Http/ApiController.php
+++ src/Infrastructure/Http/ApiController.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class ApiController extends AbstractController
 {
@@ -20,17 +22,28 @@ class ApiController extends AbstractController
     #[Route('/beers', methods: ['GET'])]
     public function getBeerByFood(
         Request $request,
-        GetFilterBeerByStringUseCase $getFilterBeerByStringUseCase
+        GetFilterBeerByStringUseCase $getFilterBeerByStringUseCase,
+        CacheInterface $cache
     ): JsonResponse {
 
         try {
+            $food = str_replace(' ', '_', $request->query->get('food'));
+            $cacheKey = 'beer_cache_' . $food;
 
-            $beers = $getFilterBeerByStringUseCase->filterByString(
-                str_replace(' ', '_', $request->query->get('food'))
+            $cachedData = $cache->get(
+                $cacheKey,
+                function (ItemInterface $item) use ($getFilterBeerByStringUseCase, $food) {
+
+                    $beers = $getFilterBeerByStringUseCase->filterByString($food);
+
+                    $item->expiresAfter(3600);
+
+                    return $beers;
+                }
             );
 
             return $this->json([
-                'data' => $this->serializeObjects($beers)
+                'data' => $this->serializeObjects($cachedData)
             ], Response::HTTP_OK);
         } catch (BadRequestException $e) {
             return $this->json(['msg' => $e->getMessage()], $e->getCode());
@@ -44,22 +57,34 @@ class ApiController extends AbstractController
     #[Route('/beer/{id}', methods: ['GET'])]
     public function getBeerById(
         Request $request,
-        GetFilterBeerByIdUseCase $getFilterBeerByIdUseCase
+        GetFilterBeerByIdUseCase $getFilterBeerByIdUseCase,
+        CacheInterface $cache
     ): JsonResponse {
 
         try {
-           
-            $beer = $getFilterBeerByIdUseCase->filterById(
-                intval($request->attributes->get('id'))
+
+            $id = intval($request->attributes->get('id'));
+            $cacheKey = 'beer_cache_' . $id;
+
+            $beerCatched = $cache->get(
+                $cacheKey,
+                function (ItemInterface $item) use ($getFilterBeerByIdUseCase, $id) {
+
+                    $beer = $getFilterBeerByIdUseCase->filterById($id);
+
+                    $item->expiresAfter(3600);
+
+                    return $beer;
+                }
             );
 
             return $this->json([
-                'id' => $beer->getId(),
-                'name' => $beer->getName(),
-                'tagline' => $beer->getTagline(),
-                'first_brewed' => $beer->getFirstBrewed(),
-                'description' => $beer->getDescription(),
-                'image' => $beer->getImage()
+                'id' => $beerCatched->getId(),
+                'name' => $beerCatched->getName(),
+                'tagline' => $beerCatched->getTagline(),
+                'first_brewed' => $beerCatched->getFirstBrewed(),
+                'description' => $beerCatched->getDescription(),
+                'image' => $beerCatched->getImage()
             ], Response::HTTP_OK);
         } catch (BadRequestException $e) {
             return $this->json(['msg' => $e->getMessage()], $e->getCode());
